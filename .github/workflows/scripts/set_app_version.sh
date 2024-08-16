c5a542cc9931b7d0b928eac4eb13b7ad01dcc596
Elvis Ngatia <ngatiadyte@gmail.com>
2024-08-16 09:46:40 +0300

    Update set_app_version.sh

--- .github/workflows/scripts/set_app_version.sh
+++ .github/workflows/scripts/set_app_version.sh
@@ -7,7 +7,7 @@
 function set_app_version() {
     echo 'Setting app version'
     # TODO: verify where sh will run to update build number
-    build_number=$(echo $(grep "^version:" ../../../pubspec.yaml | sed 's/.*+\(.*\)/\1/' | awk '{print $1 + 1}'))
+    build_number=$(echo $(grep "^version:" pubspec.yaml | sed 's/.*+\(.*\)/\1/' | awk '{print $1 + 1}'))
     # version=$(echo "${github.event.release.tag_name}" | cut -d 'v' -f2)
     echo "APP_VERSION=$version"
     echo "BUILD_NUMBER=$build_number"
